9da02adb43c5d6dd8573f7f3e44da47ec8aaf332
nitin-vashishtha8 <nitinsharma8224@gmail.com>
2025-04-05 14:08:24 +0530

    final look

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Blogs from "./Blogs.json";
 import "./App.css"; 
+import menu from "./assets/menu.svg"
 
 
 function App() {
   return (
     
     <div className="App">
-    <nav>
-      <h1>FELLABLOGS</h1>
-      
-    </nav>
+    <div className="header">
+    <h1>FELLABLOGS</h1>
+      <img src={menu} alt="Menu Logo"/>
+      </div>
       <div className="blog-container">
         {Blogs.map((blog) => (
           <div className="blog-card" key={blog.id}>
