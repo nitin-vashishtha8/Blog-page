d8f09403f328fc80f24f83cca45f59efdda43cf5
nitin-vashishtha8 <nitinsharma8224@gmail.com>
2025-04-05 14:33:00 +0530

    Let me update it once again.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Blogs from "./Blogs.json";
-import "./App.css"; 
-import menu from "./assets/menu.svg"
-
+import "./App.css";
+import menu from "./assets/menu.svg";
 
 function App() {
-  return (
+  
+  const [visibleDescriptions, setVisibleDescriptions] = useState({});
+
+  
+  const toggleDescription = (blogId, event) => {
     
+    event.stopPropagation();
+    
+    setVisibleDescriptions(prevState => ({
+      ...prevState,
+      [blogId]: !prevState[blogId]
+    }));
+  };
+
+  return (
     <div className="App">
-    <div className="header">
-    <h1>FELLABLOGS</h1>
-      <img src={menu} alt="Menu Logo"/>
+      <div className="header">
+        <h1>FELLABLOGS</h1>
+        <img src={menu} alt="Menu Logo" />
       </div>
       <div className="blog-container">
         {Blogs.map((blog) => (
-          <div className="blog-card" key={blog.id}>
+          <div 
+            className="blog-card" 
+            key={blog.id}
+            onClick={(e) => toggleDescription(blog.id, e)}
+          >
             <h2 className="blog-title">{blog.title}</h2>
-            <br></br>
-            
-             <div className="blog-details">
-               <span className="blog-author">By {blog.author} |</span>
+            <br />
+            <div className="blog-details">
+              <span className="blog-author">By {blog.author} |</span>
               <span className="blog-date"> {blog.date}</span>
-              </div>
+            </div>
             
-            <p className="blog-description">{blog.description}</p>
+            
+            {visibleDescriptions[blog.id] && (
+              <p className="blog-description">{blog.description}</p>
+            )}
           </div>
         ))}
       </div>
@@ -32,5 +50,4 @@ function App() {
 }
 
 export default App;
-
 
