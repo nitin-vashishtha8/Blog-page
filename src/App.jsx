0cacd1a1fb27693920da0262894b7344d662b0e3
nitin-vashishtha8 <nitinsharma8224@gmail.com>
2025-04-05 13:19:16 +0530

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import Blogs from "./Blogs.json";
 import "./App.css"; 
 
+
 function App() {
   return (
     
     <div className="App">
-    <div><h1>Nit Blogs</h1></div>  
+    <nav>
+      <h1>FELLABLOGS</h1>
+      
+    </nav>
       <div className="blog-container">
         {Blogs.map((blog) => (
           <div className="blog-card" key={blog.id}>
             <h2 className="blog-title">{blog.title}</h2>
             <br></br>
             
-              <span className="blog-author">By {blog.author}</span>
-              <span className="blog-date">📅 {blog.date}</span>
+             <div className="blog-details">
+               <span className="blog-author">By {blog.author} |</span>
+              <span className="blog-date"> {blog.date}</span>
+              </div>
             
             <p className="blog-description">{blog.description}</p>
           </div>
